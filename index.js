4df89be55c93a5bd65f46736ed0103476e4829d3
Ivo Evans Storrie <ievans.storrie@gmail.com>
2023-02-10 17:23:10 +0000

    Update the browserslist config

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 module.exports = [
-  "Chrome >= 43",
-  "iOS >= 8",
-  "Safari >= 8",
-  "ie >= 11",
-  "Samsung >= 4",
+  "Safari >= 9",
+  "iOS >= 9",
+  "Chrome >= 56",
+  "Samsung >= 11",
   "Firefox ESR",
   "Firefox >= 52",
-  "Opera >= 53",
-  "Edge >= 14"
+  "Edge >= 90"
 ]
