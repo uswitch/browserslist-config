aab9bd0fa2746a35c2f93267b5d6a51ec31b9785
tobias-edwards <tobias_r_edwards@hotmail.co.uk>
2025-05-07 15:31:34 +0100

    Add esbuild target config

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ module.exports = [
   'Firefox >= 115.0',
   'Edge >= 109',
   'Android >= 109',
-]
+];
+
+// esbuild target syntax: https://esbuild.github.io/api/#target
+module.exports.esbuildTarget = [
+  'safari15.5',
+  // Esbuild does not support below es6
+  // ios10.3 does not support 'const' and requires es5
+  // ios11 is the lowest we can support for now
+  'ios11',
+  'chrome109',
+  'firefox115',
+  'edge109',
+];
